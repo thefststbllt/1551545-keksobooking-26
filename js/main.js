11131a07f51454ed259960b2eab4a4edbc3644e6
thefststbllt <bel12pasha@yandex.ru>
2022-06-13 17:43:31 +0300

    style: set in order some mistakes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-// Example of the function below was gently peeked from https://learn.javascript.ru/task/random-int-min-max
+const SIMILAR_ADVERT_COUNT = 10;
+const PLACE_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME_LIST = ['12:00', '13:00', '14:00'];
+const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS_LIST = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',];
+const MIN_LATITUDE = 35.65;
+const MAX_LATITUDE = 35.7;
+const MIN_LONGITUDE = 139.7;
+const MAX_LONGITUDE = 139.8;
 
-function getRandomInteger(min, max) {
+// Auxiliary function for getting a random integer number within some range
+
+const getRandomInteger = (min, max) => {
   if (min >= 0 && max >= 0) {
     if (min > max) {
       return undefined;
@@ -12,15 +22,11 @@ function getRandomInteger(min, max) {
     return Math.floor(rand);
   }
   return undefined;
-}
+};
 
-getRandomInteger(5, 200);
+// Auxiliary function for getting a random float number
 
-/* Example of the function below was gently peeked from https://learn.javascript.ru/task/random-min-max &
- https://javascript.ru/forum/misc/2860-okruglenie-do-nuzhnogo-kolichestva-znakov-posle-zapyatojj.html
-*/
-
-function getRandomFlops(min, max, symbols) {
+const getRandomFlops = (min, max, symbols) => {
   if (min >= 0 && max >= 0) {
     if (min > max) {
       return undefined;
@@ -36,64 +42,51 @@ function getRandomFlops(min, max, symbols) {
     return Math.round(randomNumber * m) / m;
   }
   return undefined;
-}
-
-getRandomFlops(0, 1, 1);
+};
 
 // HOMEWORK module4-task1 branch
 
-const SIMILAR_ADVERT_COUNT = 10;
-const placeType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkTime = ['12:00', '13:00', '14:00'];
-const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photosList = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',];
-
 const generatedValues = [];
 
-function getArray() {
+const getArray = () => {
   while (generatedValues.length < SIMILAR_ADVERT_COUNT) {
     const value = getRandomInteger(1, SIMILAR_ADVERT_COUNT);
     if (!generatedValues.includes(value)) {
       generatedValues.push(value);
     }
   }
-}
+};
 
-function getAvatarArrayElements() {
+const getAvatarArrayElements = () => {
   const orderNumber = (`0${  generatedValues.shift()}`).slice(-2);
   return `img/avatars/user${  String(orderNumber)  }.png`;
-}
+};
 
-function createAdvert() {
-  return {
-    author: {avatar: getAvatarArrayElements()},
-    offer: {
-      title: 'Offer',
-      address: 'location.lat, location.lng',
-      price: getRandomInteger(100, 50000),
-      type: placeType[getRandomInteger(0, placeType.length - 1)],
-      rooms: getRandomInteger(1, 10),
-      guests: getRandomInteger(1, 10),
-      checkin: checkTime[getRandomInteger(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomInteger(0, checkTime.length - 1)],
-      features: featuresList.slice(0, getRandomInteger(1, featuresList.length)),
-      description: 'Best offer you ve ever get!',
-      photos: photosList.slice(0, getRandomInteger(1, photosList.length)),
-    },
-    location: {
-      lat: getRandomFlops(35.65, 35.7, 8),
-      lng: getRandomFlops(139.7, 139.8, 8),
-    },
-  };
-}
+const createAdvert = () => ({
+  author: {avatar: getAvatarArrayElements()},
+  offer: {
+    title: 'Offer',
+    address: 'location.lat, location.lng',
+    price: getRandomInteger(100, 50000),
+    type: PLACE_TYPE[getRandomInteger(0, PLACE_TYPE.length - 1)],
+    rooms: getRandomInteger(1, 10),
+    guests: getRandomInteger(1, 10),
+    checkin: TIME_LIST[getRandomInteger(0, TIME_LIST.length - 1)],
+    checkout: TIME_LIST[getRandomInteger(0, TIME_LIST.length - 1)],
+    features: FEATURES_LIST.slice(0, getRandomInteger(1, FEATURES_LIST.length)),
+    description: 'Best offer you ve ever get!',
+    photos: PHOTOS_LIST.slice(0, getRandomInteger(1, PHOTOS_LIST.length)),
+  },
+  location: {
+    lat: getRandomFlops(MIN_LATITUDE, MAX_LATITUDE, 8),
+    lng: getRandomFlops(MIN_LONGITUDE, MAX_LONGITUDE, 8),
+  },
+});
 
 //Array generating
 getArray();
 const advertList = Array.from({length: SIMILAR_ADVERT_COUNT}, createAdvert);
 
-function returnAdvertList () {
-  return advertList;
-}
+const returnAdvertList = () => advertList;
 
 returnAdvertList();
-
